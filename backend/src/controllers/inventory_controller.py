5e992476698ad5d0ffab7fbd136ad20cd89c2b04
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2025-04-25 11:07:27 +0530

    Did changes

--- backend/src/controllers/inventory_controller.py
+++ backend/src/controllers/inventory_controller.py
@@ -1,36 +1,3 @@
-# import csv
-# from src.models.inventory_history import InventoryHistory
-# from src import db
-
-# def import_inventory_data_from_csv(csv_file_path):
-#     with open(csv_file_path, newline='', encoding='utf-8-sig') as csvfile:
-#         reader = csv.DictReader(csvfile, delimiter='\t')  # or ',' if needed
-
-#         for row in reader:
-#             row = {k.strip(): v for k, v in row.items()}
-
-#             inventory = InventoryHistory(
-#                 ASSY_Part_Number=row.get("ASSY_Part _Number", ""),
-#                 SUBASSY_Part_Number=row.get("SUBASSY_Part_Number", ""),
-#                 Manufacturer=row.get("Manufacturer", ""),
-#                 Shipping_Classification=row.get("Shipping_Classification", ""),
-#                 Airtightness_Inspection=int(row.get("Airtightness_Inspection", 0) or 0),
-#                 SCU=int(row.get("SCU", 0) or 0),
-#                 Water_Vapor_Test=int(row.get("Water_Vapor_Test", 0) or 0),
-#                 Characteristic_Inspection=int(row.get("Characteristic_Inspection", 0) or 0),
-#                 Characteristic_Inspection_Fractional_Items=int(row.get("Characteristic_Inspection_Fractional_Items", 0) or 0),
-#                 Accessories=int(row.get("Accessories", 0) or 0),
-#                 FA=int(row.get("FA", 0) or 0),
-#                 FA_Fractional_Items=int(row.get("FA_Fractional_Items", 0) or 0),
-#                 Visual_Inspection=int(row.get("Visual_Inspection", 0) or 0),
-#                 Update_Date_And_Time=row.get("Update_Date_And _Time", "")
-#             )
-
-#             db.session.add(inventory)
-
-#         db.session.commit()
-#         return f"✅ {reader.line_num - 1} records imported successfully."
-
 import pandas as pd
 from src import db
 from src.models.inventory_history import InventoryHistory
